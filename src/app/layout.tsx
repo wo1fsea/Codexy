16828d5123c06008abb4c7b4961ea6451b95751d
Quanyong Huang <quanyongh@foxmail.com>
2026-04-02 12:25:29 +0800

    chore: update app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,9 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  viewportFit: "cover",
   colorScheme: "dark",
   themeColor: "#141413"
 };
